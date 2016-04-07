12a667b0c9f72515089e9a5285e0e578e98ba479
qqumn <qqumn@icloud.com>
2016-04-07 17:31:51 +0300

    Flexible aline for blog posts

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 
     $("a#single_image").fancybox();
 
+    alert('dfghjkl');
+
     /* Using custom settings */
 
     $("a#inline").fancybox({
@@ -12,6 +14,7 @@ $(document).ready(function () {
 
     /* Apply fancybox to multiple items */
 
+
     $("a.group").fancybox({
         'transitionIn': 'elastic',
         'transitionOut': 'elastic',
@@ -19,5 +22,4 @@ $(document).ready(function () {
         'speedOut': 200,
         'overlayShow': false
     });
-
 });
