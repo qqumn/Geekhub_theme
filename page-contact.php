cc2eadae7e861cb86adb2d09566edb4c5953066b
ajcpwnz <fentg1@hotmail.com>
2016-04-08 18:46:00 +0300

    Contact form short co de

--- page-contact.php
+++ page-contact.php
@@ -2,27 +2,28 @@
 /*Template Name: Contact*/
 
 get_header(); ?>
-        <div class="container-elastic">
-            <?php
-            get_sidebar();
-            ?>
-            <?php echo get_theme_mod('address-iframe', ''); ?>
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main map-container" role="main">
-                            <ul class="contact-info">
-                                    <li id="contact-name"><?php echo get_theme_mod('name', ''); ?></li>
-                                    <li id="contact-address"><a
-                                                    href="<?php echo get_theme_mod('address-map', ''); ?>"><?php echo get_theme_mod('address', ''); ?></a>
-                                        </li>
-                                    <li id="contact-mail"><a
-                                                    href="mailto:<?php echo get_theme_mod('mail', ''); ?>"><?php echo get_theme_mod('mail', ''); ?></a>
-                                        </li>
-                                </ul>
-                            <div class="contact-form">
-                            </div>
-                        </main><!-- #main -->
-                </div><!-- #primary -->
+    <div class="container-elastic">
+        <?php
+        get_sidebar();
+        ?>
+        <?php echo get_theme_mod('address-iframe', ''); ?>
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main map-container" role="main">
+                <ul class="contact-info">
+                    <li id="contact-name"><?php echo get_theme_mod('name', ''); ?></li>
+                    <li id="contact-address"><a
+                            href="<?php echo get_theme_mod('address-map', ''); ?>"><?php echo get_theme_mod('address', ''); ?></a>
+                    </li>
+                    <li id="contact-mail"><a
+                            href="mailto:<?php echo get_theme_mod('mail', ''); ?>"><?php echo get_theme_mod('mail', ''); ?></a>
+                    </li>
+                </ul>
+                <div class="contact-form">
+                    <?php echo do_shortcode('[formCustom]') ?>
+                </div>
+            </main><!-- #main -->
+        </div><!-- #primary -->
 
-        </div>
-    <?php
+    </div>
+<?php
 get_footer();
