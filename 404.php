2077bb2d5097e1a1807c555139a8b00cbc6e223a
YuriyChaban <shuyterrush@gmail.com>
2016-04-10 19:23:01 +0300

    styling 404.php

--- 404.php
+++ 404.php
@@ -8,57 +8,29 @@
  */
 
 get_header(); ?>
+    <div class="container-elastic">
+        <?php
+        get_sidebar();
+        ?>
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+                <section class="error-404 not-found">
+                    <header class="page-header">
+                        <h1 class="page-title"><?php esc_html_e('404 - Oops!', 'ghtmeme'); ?></h1>
+                    </header><!-- .page-header -->
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ghtmeme' ); ?></h1>
-				</header><!-- .page-header -->
+                    <div class="page-content">
+                        <p class="main-information"><?php _e('The page you are trying to reach can&apos;t be found', 'ghtmeme'); ?></p>
+                    <p class="addition-information"><?php _e('Try refining your search, or use the navigation above to locate the post.', 'liquidfolio'); ?></p>
+                    <p class="back-home"><a href="<?php echo get_home_url() ?>"
+                        <a href="<?php echo get_home_url() ?>"><?php _e('back to home', 'ghtmeme'); ?></a>
+                    </p>
+        </div><!-- .page-content -->
+        </section><!-- .error-404 -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ghtmeme' ); ?></p>
-
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( ghtmeme_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ghtmeme' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ghtmeme' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+        </main><!-- #main -->
+    </div><!-- #primary -->
+</div>
 <?php
 get_footer();
